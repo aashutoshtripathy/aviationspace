469c1f6f2f7db6eb58e3f0fe703b604860875f47
Ashutosh <ashutoshkmr419@gmail.com>
2024-06-24 16:07:19 +0530

    frontend/src/data/CoursalImg.js

--- frontend/src/data/CoursalImg.js
+++ frontend/src/data/CoursalImg.js
@@ -1,49 +1,8 @@
-import image1 from '../assets/diamond.png';
-import image2 from '../assets/DQS Final.jpg';
-import image3 from '../assets/Logo_2.jpg';
-import image4 from '../assets/NDT Logo 2.jpg';
-// import image5 from '';
-// import image6 from '';
-// import image7 from '';
-// import image8 from '';
-// import image9 from '';
-// import image10 from '';
+// src/data/CoursalImg.js
+// import images from '../components/image.jsx';
+
+// const CoursalImg = images.map((src, index) => ({ src }));
+
+// export default CoursalImg;
 
 
-
-const CoursalImg = [
-    {
-        src: image1
-    },
-    {
-        src: image2
-    },
-    {
-        src: image3
-    },
-    {
-        src: image4
-    },
-    // {
-    //     img: image5
-    // },
-    // {
-    //     img: image6
-    // },
-    // {
-    //     img: image7
-    // },
-    // {
-    //     img: image8
-    // },
-    // {
-    //     img: image9
-    // },
-    // {
-    //     img: image10
-    // }
-]
-
-
-
-export default CoursalImg;
