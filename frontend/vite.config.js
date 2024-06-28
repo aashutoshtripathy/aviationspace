a0b728e2c95cc5b09f6ccff9f8bec08c76bb8145
Ashutosh <ashutoshkmr419@gmail.com>
2024-06-28 13:47:30 +0530

    modified

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,53 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:8000', // Base URL of your API server
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, '/api/v1/users')
+//       }
+//     }
+//   }
+// });
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
 
-// https://vitejs.dev/config/
+// Load environment variables from .env file
+dotenv.config();
+
+// console.log('VITE_API_BASE_URL:',  import.meta.VITE_API_BASE_URL);
+
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:8000',
+//         // target: import.meta.VITE_API_BASE_URL, // Use process.env to access the environment variable
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, '/api/v1')
+//       }
+//     }
+//   }
+// });
+
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000', // Base URL of your API server
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api/v1/users')
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:8000', // Base URL of your API server
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '/api/v1/users')
+        }
       }
     }
-  }
-});
+  });
+
