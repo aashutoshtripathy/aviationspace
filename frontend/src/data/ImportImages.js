bad3bab73eb464e88e9e2d880cd5110b474e1b25
Ashutosh <ashutoshkmr419@gmail.com>
2024-06-29 10:02:13 +0530

    added

---
+++ frontend/src/data/ImportImages.js
@@ -0,0 +1,6 @@
+// src/data/importImages.js
+// const importAll = (r) => r.keys().map((key) => r(key).default);
+
+// const images = importAll(require.context('../assets/coursal', false, /\.(png|jpe?g|svg)$/));
+
+// export default images;
