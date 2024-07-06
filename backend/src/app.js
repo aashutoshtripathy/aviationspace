b9baa3e291ee52f0ae19a2521847abf304e45631
Ashutosh <ashutoshkmr419@gmail.com>
2024-07-06 12:25:25 +0530

    modified

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,54 +2,49 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import nodemailer from "nodemailer";
-import path from 'path';
-// import { fileURLToPath } from './assets/cousral';
-import { fileURLToPath } from 'url';
-
-
-
-
+import path from "path";
+import { fileURLToPath } from "url";
+import multer from "multer";
 
 const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    Credential: true
-}))
+    Credential: true,
+  })
+);
 
-app.use(express.json({limit:"16kb"}))
+app.use(express.json({ limit: "16kb" }));
 
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-// app.use(express.static())
-app.use(cookieParser())
-// app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(cookieParser());
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+console.log(__dirname);
 
-app.use('/downloads', express.static(path.join(__dirname, 'downloads')));
+app.use("/cousral", express.static(path.join(__dirname, "/assets/cousral")));
+app.use("/assets", express.static(path.join(__dirname, "assets")));
+app.use("/classifiedImages", express.static(path.join(__dirname, "/assets/classifiedImages")));
+app.use("/newsImages", express.static(path.join(__dirname, "/assets/newsImages")));
+
 
 
 
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
-
-
-
-
-import userRouter from "./routes/user.routes.js"
+import userRouter from "./routes/user.routes.js";
 import bodyParser from "body-parser";
 import verifyToken from "./middleware/auth.middleware.js";
 
-
-app.use("/api/v1/users" , userRouter)
-
+app.use("/api/v1/users", userRouter);
 
 export { app };
